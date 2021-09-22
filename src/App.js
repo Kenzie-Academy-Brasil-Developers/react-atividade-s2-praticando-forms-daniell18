84a481593a973904d18ad5b80e173d9762486149
Daniel Francisco <daniel_mp18@outlook.com>
2021-09-22 16:38:02 -0300

    final

--- src/App.js
+++ src/App.js
@@ -2,8 +2,11 @@ import "./App.css";
 import * as yup from "yup";
 import { useForm } from "react-hook-form";
 import { yupResolver } from "@hookform/resolvers/yup";
+import { useState } from "react";
 
 function App() {
+  const [isTrue, setIsTrue] = useState(true);
+  const [card, setCard] = useState();
   const schema = yup.object().shape({
     name: yup.string().required("Nome Obrigatorio"),
     Email: yup.string().required("Email obrigatorio").email("Email invalido"),
@@ -19,7 +22,7 @@ function App() {
     confirmedPassword: yup
       .string()
       .required("Campo Obrigatorio")
-      .oneOf([yup.ref("password"), null], "Senhas diferente"),
+      .oneOf([yup.ref("password"), null], "Senhas diferentes"),
     user: yup.string().required("Nome de usuario Obrigatorio"),
   });
 
@@ -30,48 +33,70 @@ function App() {
   } = useForm({
     resolver: yupResolver(schema),
   });
-  const onSubmitFunction = (e) => {};
-  console.log(schema);
+  const onSubmitFunction = (e) => {
+    setCard(e);
+    setIsTrue(false);
+  };
+
   return (
     <div className="App">
       <>
-        <h3>Formulario</h3>
-        <form className="form" onSubmit={handleSubmit(onSubmitFunction)}>
-          <div className="info">
-            <input placeholder="Nome" {...register("name")} />
-            <p>{errors.name?.message}</p>
+        {isTrue ? (
+          <div>
+            <h3>Formulario</h3>
+            <form className="form" onSubmit={handleSubmit(onSubmitFunction)}>
+              <div className="info">
+                <input placeholder="Nome" {...register("name")} />
+                <p>{errors.name?.message}</p>
+              </div>
+              <div className="info">
+                <input placeholder="Usuario" {...register("user")} />
+                <p>{errors.user?.message}</p>
+              </div>
+              <div className="info">
+                <input placeholder="Email" {...register("Email")} />
+                <p>{errors.Email?.message}</p>
+              </div>
+              <div className="info">
+                <input placeholder="Telefone" {...register("cellphone")} />
+                <p> {errors.cellphone?.message}</p>
+              </div>
+              <div className="info">
+                <input placeholder="Endereço" {...register("address")} />
+                <p>{errors.address?.message}</p>
+              </div>
+              <div className="info-senha">
+                <div className="info">
+                  <input
+                    placeholder="Senha"
+                    type="password"
+                    {...register("password")}
+                  />
+                  <p>{errors.password?.message} </p>
+                </div>
+                <div className="info">
+                  <input
+                    type="password"
+                    placeholder="Confirmar Senha"
+                    {...register("confirmedPassword")}
+                  />
+                  <p> {errors.confirmedPassword?.message}</p>
+                </div>
+              </div>
+              <button type="submit">Enviar </button>
+            </form>{" "}
           </div>
-          <div className="info">
-            <input placeholder="Usuario" {...register("user")} />
-            <p>{errors.user?.message}</p>
-          </div>
-          <div className="info">
-            <input placeholder="Email" {...register("Email")} />
-            <p>{errors.Email?.message}</p>
-          </div>
-          <div className="info">
-            <input placeholder="Telefone" {...register("cellphone")} />
-            <p> {errors.cellphone?.message}</p>
-          </div>
-          <div className="info">
-            <input placeholder="Endereço" {...register("address")} />
-            <p>{errors.address?.message}</p>
-          </div>
-          <div className="info-senha">
-            <div className="info">
-              <input placeholder="Senha" {...register("password")} />
-              <p>{errors.password?.message} </p>
+        ) : (
+          <div className="card  ">
+            <div className="card-info">
+              <h3>Dados Cadastrados</h3>
+              <div className="border">Nome:{card.name}</div>
+              <div className="border">Email:{card.Email}</div>
+              <div className="border">Telefone:{card.cellphone}</div>
+              <div className="border">Endereço{card.address}</div>
             </div>
-            <div className="info">
-              <input
-                placeholder="Confirmar Senha"
-                {...register("confirmedPassword")}
-              />
-              <p> {errors.confirmedPassword?.message}</p>
-            </div>
           </div>
-          <button type="submit">Enviar </button>
-        </form>
+        )}
       </>
     </div>
   );
